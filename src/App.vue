61d4db5d9d49ddc224ffba3e602670339e37d309
Noloafing <1845008982@qq.com>
2023-01-11 10:52:11 +0800

    day1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+<!-- 路由组件位置-->
+    <router-view></router-view>
+<!-- v-show绑定了路由的isShow属性 是为了事路由为登录和注册时 Footer组件不显示-->
+    <Footer v-show="$route.meta.isShow"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from "@/components/Header/index.vue";
+import Footer from "@/components/Footer/index.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
+  },
+  mounted() {
+    console.log('@route',this.$route)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
